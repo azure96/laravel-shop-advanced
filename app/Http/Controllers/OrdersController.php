fd4b9d772092062a37cd010fe01f796ac38deb43
azure96 <243446366@qq.com>
2018-09-24 03:20:21 +0000

    众筹商品详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SendReviewRequest;
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Http\Requests\CrowdfundingOrderRequest;
 use App\Models\UserAddress;
 use App\Models\Order;
+use App\Models\CouponCode;
+use App\Models\ProductSku;
 use Illuminate\Http\Request;
 use App\Services\OrderService;
 use App\Events\OrderReviewd;
 use App\Http\Requests\ApplyRefundRequest;
 use App\Exceptions\CouponCodeUnavailableException;
-use App\Models\CouponCode;
 use Carbon\Carbon;
 
 class OrdersController extends Controller
@@ -134,5 +136,16 @@ class OrdersController extends Controller
         ]);
 
         return $order;
+    }
+
+    // 创建一个新的方法用于接受众筹商品下单请求
+    public function crowdfunding(CrowdfundingOrderRequest $request, OrderService $orderService)
+    {
+        $user = $request->user();
+        $sku = ProductSku::find($request->input('sku_id'));
+        $address = UserAddress::find($request->input('address_id'));
+        $amount = $request->input('amount');
+
+        return $orderService->crowdfunding($user, $address, $sku, $amount);
     }
 }
