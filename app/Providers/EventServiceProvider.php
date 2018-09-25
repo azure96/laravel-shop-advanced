ff57b0ba291c974200e824b26f4cb1c926bc6062
azure96 <243446366@qq.com>
2018-09-25 02:15:24 +0000

    订单模块调整

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -8,6 +8,7 @@ use App\Events\OrderPaid;
 use App\Listeners\UpdateProductSoldCount;
 use App\Listeners\SendOrderPaidMail;
 use App\Listeners\RegisteredListener;
+use App\Listeners\UpdateCrowdfundingProductProgress;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -26,6 +27,7 @@ class EventServiceProvider extends ServiceProvider
         OrderPaid::class => [
             UpdateProductSoldCount::class,
             SendOrderPaidMail::class,
+            UpdateCrowdfundingProductProgress::class,
         ],
         OrderReviewd::class => [
             UpdateProductRating::class,
